eaa1d953595022b47f41e64fff69d3174653432e
Mariusz <mpostol@cas.eu>
2016-10-09 19:41:14 +0000

    CommServer.UA.Viewer - release - working on installer.  - added Unit Tests - passed  - open issues - setup doesn't work. ClickOnce need installation configuration redevelopment, InstallShield needs solving custom action. Issue #: 4867

--- Viewer.UserInterface/Properties/AssemblyInfo.cs
+++ Viewer.UserInterface/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿//_______________________________________________________________
-//  Title   :  Assembly info for: CAS.CommServer.UA.Viewer.UserInterface
+//  Title   :  Assembly info for: CAS.CommServer.UA.Viewer.UserInterface.UnitTest
 //  System  : Microsoft VisualStudio 2015 / C#
 //  $LastChangedDate$
 //  $Rev$
@@ -17,7 +17,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CAS.CommServer.UA.Viewer.UserInterface")]
+[assembly: AssemblyTitle("CAS.CommServer.UA.Viewer.UserInterface.UnitTest")]
 [assembly: AssemblyDescription("CAS OPC UA Viewer User Interface")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
@@ -29,4 +29,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.00.14.*")]
 [assembly: AssemblyFileVersion("1.00.14")]
+[assembly: InternalsVisibleToAttribute("CAS.CommServer.UA.Viewer.UserInterfaceUnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6" +
+                                                                       "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d" +
+                                                                       "d9ad606bd9f4e0a8e1b0c49fa37835630b16798ece56df81c756f4c6bd37f07d68cc91ebfa7be7" +
+                                                                       "8149d016bbc6a364beda14fc1119698d8c8d9923805b40122bf594f08cc29f41cb0a31e5e141e3" +
+                                                                       "f2421b9d", AllInternalsVisible = true)]
 
---
+++ Viewer.UserInterfaceUnitTest/ProgramUnitTest.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CAS.CommServer.UA.Viewer.UserInterface.UnitTest
+{
+  [TestClass]
+  public class ProgramUnitTest
+  {
+    [TestMethod]
+    public void AssemblyLoadTestMethod()
+    {
+      Type _ProgramType = typeof(Program);
+      Assembly _opcViewer = _ProgramType.Assembly;
+      Assert.IsNotNull(_opcViewer);
+    }
+    [TestMethod]
+    public void DoInstallLicenseLocalTestMethod()
+    {
+      FileInfo _resourcesFile = new FileInfo(@"CAS.CommonResources.dll");
+      Assert.IsTrue(_resourcesFile.Exists);
+      string _lastMessage = null;
+      Program.MessageBoxShow = x => { _lastMessage = x; return DialogResult.OK; };
+      Program.DoInstallLicense(false);
+      Assert.IsNull(_lastMessage, _lastMessage);
+    }
+    [TestMethod]
+    public void DoApplicationRunTestMethod()
+    {
+      bool _isCalled = false;
+      Program.DoApplicationRun(x => { _isCalled = true; Assert.IsNull(x); });
+      Assert.IsTrue(_isCalled);
+    }
+  }
+}
