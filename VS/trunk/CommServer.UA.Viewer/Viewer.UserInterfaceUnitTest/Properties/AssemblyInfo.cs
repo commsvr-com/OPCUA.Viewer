dbf89b5c12d7748f5d3ccdd5726c6941b5bcbfe7
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-10-09 19:41:14 +0000

    CommServer.UA.Viewer - release - working on installer.  - added Unit Tests - passed  - open issues - setup doesn't work. ClickOnce need installation configuration redevelopment, InstallShield needs solving custom action. Issue #: 4867

---
+++ VS/trunk/CommServer.UA.Viewer/Viewer.UserInterfaceUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.CommServer.UA.Viewer.UserInterface
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.UA.Viewer.UserInterface")]
+[assembly: AssemblyDescription("CAS OPC UA Viewer User Interface Unit Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("fd99e534-1ec3-48a4-8b0d-0f93827ea8e5")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.00.14.*")]
+[assembly: AssemblyFileVersion("1.00.14")]
+
