6511117c81c5655cdfcc6d9438dc465dbbba3c1c
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-11-25 17:41:44 +0000

    OPC.UA.SDK - remove to separate projects relevant assemblies from the Stack - working on CommServer.UA.Viewer - creating scripts project copied from CommServer.UA.Stack.Core Issue #: 4886

---
+++ VS/trunk/CommServer.UA.Viewer/Viewer.Scripts/CreateTag.cmd
@@ -0,0 +1,13 @@
+
+rem//  $LastChangedDate:  $
+rem//  $Rev:  $
+rem//  $LastChangedBy:  $
+rem//  $URL:  $
+rem//  $Id:  $
+
+
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3263  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3263"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.UA.Stack.Core/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3263 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3263"
+
+pause ....
+
---
+++ VS/trunk/CommServer.UA.Viewer/Viewer.Scripts/CreateTagPrototyping.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CASCommServer.UA.Stack.Core.Scripts
+{
+
+  [TestClass]
+  public class CreateTagPrototyping
+  {
+
+    [TestMethod]
+    public void CreateTagTest()
+    {
+      string _version = "2.01.1061"; //random version for testing purpose only
+
+      #region Copy to script
+      string _rel = _version.Replace(".", "_"); // _version must be replaced by the correct version variable depending on the project.
+      string _repositoryUrl = @"svn://svnserver.hq.cas.com.pl/VS/";
+      string _solutionPath = @"/CommServer.UA.Stack.Core";
+      string _trunkPath = $@"{_repositoryUrl}trunk{_solutionPath}/";
+      string _tagPath = $@"{_repositoryUrl}tags{_solutionPath}.rel_{_rel}";
+      #endregion
+
+      Assert.AreEqual("svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.UA.Stack.Core/", _trunkPath);
+      Assert.AreEqual("svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_2_01_1061", _tagPath);
+
+    }
+  }
+
+}
