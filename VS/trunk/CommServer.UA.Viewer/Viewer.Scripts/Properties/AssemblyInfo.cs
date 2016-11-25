beac41bd73bcbfac62c27b1353f4b7821c7d8a76
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-11-25 17:40:34 +0000

    OPC.UA.SDK - remove to separate projects relevant assemblies from the Stack - working on CommServer.UA.Viewer - creating scripts project copied from CommServer.UA.Stack.Core Issue #: 4886

---
+++ VS/trunk/CommServer.UA.Viewer/Viewer.Scripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.CommServer.UA.Stack.Core
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.commsvr.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.UA.Stack.Core")]
+[assembly: AssemblyDescription("Scripts Prototyping Workspace for CAS.CommServer.UA.Stack.Core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("3CCC345D-BCBF-457C-A380-550CA278F75C")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.01.3263.*")]
+[assembly: AssemblyFileVersion("1.01.3263")]
+
